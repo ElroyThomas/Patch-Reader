828fb4029be93db6b250d4b6c24fbee9be5882c3
Elroy Thomas <elroypt@gmail.com>
2020-01-11 10:29:14 +0530

    First Commit

---
+++ PatchReader/PatchReader.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PatchReader
+{
+    partial class PatchReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatchReader));
+            this.opnDlgFileReader = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtCopy = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // opnDlgFileReader
+            // 
+            this.opnDlgFileReader.FileName = "opnDlgFileReader";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.lstFiles);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(610, 159);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtCopy);
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnBrowse);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(3, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(604, 51);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnClear.Location = new System.Drawing.Point(78, 18);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 30);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.AutoSize = true;
+            this.btnBrowse.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnBrowse.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.btnBrowse.Location = new System.Drawing.Point(3, 18);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 30);
+            this.btnBrowse.TabIndex = 8;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 16;
+            this.lstFiles.Location = new System.Drawing.Point(3, 18);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(604, 84);
+            this.lstFiles.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtOutput);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 159);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(610, 309);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Location = new System.Drawing.Point(3, 18);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(604, 288);
+            this.txtOutput.TabIndex = 7;
+            // 
+            // txtCopy
+            // 
+            this.txtCopy.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtCopy.Location = new System.Drawing.Point(153, 18);
+            this.txtCopy.Name = "txtCopy";
+            this.txtCopy.Size = new System.Drawing.Size(75, 30);
+            this.txtCopy.TabIndex = 10;
+            this.txtCopy.Text = "Copy";
+            this.txtCopy.UseVisualStyleBackColor = true;
+            this.txtCopy.Click += new System.EventHandler(this.txtCopy_Click);
+            // 
+            // PatchReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(610, 468);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PatchReader";
+            this.Text = "Patch Reader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog opnDlgFileReader;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBrowse;
+        public System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button txtCopy;
+
+    }
+}
+
---
+++ PatchReader/PatchReader.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatchReader
+{
+    public partial class PatchReader : Form
+    {
+        StringBuilder files;
+        List<string> filesChanged;
+        public PatchReader()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            opnDlgFileReader = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "patch",
+                Filter = "patch files (*.patch)|*.patch",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true,
+                Multiselect = true
+            };
+
+            if (opnDlgFileReader.ShowDialog() == DialogResult.OK)
+            {
+                lstFiles.Items.Clear();
+                txtOutput.Clear();
+
+                files = new StringBuilder();
+                filesChanged = new List<string>();
+                
+                foreach (String file in opnDlgFileReader.FileNames)
+                {
+
+                    lstFiles.Items.Add(file);
+                    string line = file;
+                    var type = line.GetType();
+
+                    string result = ReadFile(line);
+                }
+
+                if (!string.IsNullOrEmpty(files.ToString()))
+                {
+                    txtOutput.Text = files.ToString().Trim();
+                }
+                else
+                {
+                    MessageBox.Show("File is Empty");
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstFiles.Items.Clear();
+            txtOutput.ResetText();
+        }
+
+        private void txtCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtOutput.Text))
+            {
+                Clipboard.SetText(txtOutput.Text);
+                MessageBox.Show("Copied");
+            }
+            else
+            {
+                MessageBox.Show("Text Box is Empty");
+            }
+        }
+
+        #region Custom Methods
+        public string ReadFile(string filePath)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(filePath))
+                {
+                    string ln;
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        if (ln.Contains("(working copy)"))
+                        {
+                            ln = ln.Replace("(working copy)", "").Replace("+++ ", "").Trim();
+
+                            if (!files.ToString().Contains(ln) && !filesChanged.Any(ln.Contains))
+                            {
+                                files.AppendLine(ln);
+                                filesChanged.Add(ln);
+                            }
+                        }
+                    }
+                    file.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return files.ToString();
+        }
+        #endregion Custom Methods
+    }
+}
